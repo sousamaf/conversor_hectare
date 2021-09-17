b45e454dd9bd6bb79e203dfe1d0490cfe6dba6b0
Marco Sousa <marco.volare@gmail.com>
2021-09-17 08:39:47 -0300

    Inclusão da unidade metros.

--- conversor.py
+++ conversor.py
@@ -1,7 +1,7 @@
 import re
 class Conversor:
     def __init__(self):
-        self.re_unidade_medida = "(?:ha|hectare|hectares|al|alqs|alqueire|alqueires|ac|acre|acres|milesimo|milésimo|milesimos|milésimos|lt|lts|litro|litros|milha|milhas|milha2|milha quadrada|milhas quadradas|decimo milesimo|décimo milésimo|decimos milesimos|décimos milésimos|decimo milesimos|décimo milésimos|selamim|salamim|celamim)"
+        self.re_unidade_medida = "(?:ha|hectare|hectares|al|alqs|alqueire|alqueires|ac|acre|acres|milesimo|milésimo|milesimos|milésimos|lt|lts|litro|litros|milha|milhas|milha2|milha quadrada|milhas quadradas|decimo milesimo|décimo milésimo|decimos milesimos|décimos milésimos|decimo milesimos|décimo milésimos|selamim|salamim|celamim|metro|metros|metro2|metros2|metro quadrado|metros quadrados)"
         self.re_unidade_hectare = "(?:ha|hectare|hectares)"
         self.re_unidade_acre = "(?:ac|acre|acres)"
         self.re_unidade_alqueire = "(?:al|alqs|alqueire|alqueires)"
@@ -10,6 +10,7 @@ class Conversor:
         self.re_unidade_decimo_milesimo = "(?:decimo milesimo|décimo milésimo|decimos milesimos|décimos milésimos|decimo milesimos|décimo milésimos)"
         self.re_unidade_milha = "(?:milha|milhas|milha2|milha quadrada|milhas quadradas)"
         self.re_unidade_salamim = "(?:selamim|salamim|celamim)"
+        self.re_unidade_metro = "(?:metro|metros|metro2|metros2|metro quadrado|metros quadrados)"
         self.re_valores = "\d+(?:\.\d*)?"
     
     def verificar_unidades(self, texto):
@@ -39,6 +40,9 @@ class Conversor:
 
         if len(re.findall(self.re_unidade_milesimo, unidade)) > 0:
             return lambda a : (a / 1000) * 2.42
+
+        if len(re.findall(self.re_unidade_metro, unidade)) > 0:
+            return lambda a : (a / 10000)
 
         if len(re.findall(self.re_unidade_milha, unidade)) > 0:
             return lambda a : a * 259.0
